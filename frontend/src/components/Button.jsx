bf4ffcfc4e76c9a2fda514311f05b047ff3f174e
Peter Egbujie <ogbillion@gmail.com>
2023-03-10 14:12:12 -0800

    Updated button and banner

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Button = () => {
  return (
   <Link passHref href="/contact">
-   <button className="px-4 py-3 font-bold text-white bg-blue-500 rounded-full">
+   <button className="px-4 py-3 font-bold text-white bg-blue-500 rounded-lg">
     (702)-625-4334
    </button>
   </Link>
