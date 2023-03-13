2e7603f9eff7afe5fe5da9b785de0640dd4e83b9
Peter Egbujie <ogbillion@gmail.com>
2023-03-13 11:39:50 -0700

    Added Scroll To Top feature

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
 import AboutSection from "@/components/AboutSection";
 import Banner from "@/components/Banner";
 import CTASection from "@/components/CTASection";
+import ScrollToTop from "@/components/ScrollToTop";
 import ServicesSection from "@/components/ServicesSection";
 
 export default function Home() {
  return (
-  <main>
+  <main className="flex flex-col min-h-screen">
    <Banner />
    <AboutSection />
    <ServicesSection />
    <CTASection />
+   <ScrollToTop />
   </main>
  );
 }
---
+++ frontend/src/components/ScrollToTop.jsx
@@ -0,0 +1,54 @@
+"use client";
+import { ArrowUpIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+
+const classNames = (...classes) => {
+ return classes.filter(Boolean).join(" ");
+};
+
+export const ScrollToTop = () => {
+ const [isVisible, setIsVisible] = useState(false);
+
+ const toggleVisibility = () => {
+  if (window.pageYOffset > 300) {
+   setIsVisible(true);
+  } else {
+   setIsVisible(false);
+  }
+ };
+
+ const scrollToTop = () => {
+  window.scrollTo({
+   top: 0,
+   behavior: "smooth",
+  });
+ };
+
+ useEffect(() => {
+  window.addEventListener("scroll", toggleVisibility);
+
+  return () => {
+   window.removeEventListener("scroll", toggleVisibility);
+  };
+ }, []);
+
+ return (
+  <div className="fixed bottom-2 right-2">
+   <button
+    type="button"
+    onClick={scrollToTop}
+    className={classNames(
+     isVisible ? "opacity-100" : "opacity-0",
+     "bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 inline-flex items-center rounded-full p-3 text-white shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2"
+    )}
+   >
+    <ArrowUpIcon
+     className="w-6 h-6 text-lg font-bold text-white"
+     aria-hidden="true"
+    />
+   </button>
+  </div>
+ );
+};
+
+export default ScrollToTop;
