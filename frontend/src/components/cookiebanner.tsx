508f6b0457359f64244b91799f64c8a90cf02c3e
Peter Egbujie <ogbillion@gmail.com>
2023-04-20 13:25:49 -0700

    Fixed google analytics consent banner error

--- frontend/src/components/cookiebanner.tsx
+++ frontend/src/components/cookiebanner.tsx
@@ -29,19 +29,18 @@ export default function CookieBanner() {
   <div
    className={`my-10 mx-auto max-w-max md:max-w-screen-sm
                         fixed bottom-0 left-0 right-0 
-                        flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                        ${
+                         cookieConsent != null ? "hidden" : "flex"
+                        } px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
                          bg-gray-700 rounded-lg shadow`}
   >
    <div className="text-center">
     <Link href="/info/cookies">
-     <p>
-      We use <span className="font-bold text-sky-400">cookies</span> on our
-      site.
-     </p>
+     <p className="text-gray-300">We use cookies on our site.</p>
     </Link>
    </div>
 
-   <div className={`${cookieConsent != null ? "hidden" : "flex"}  gap-2`}>
+   <div className="flex gap-2">
     <button
      className="px-5 py-2 text-gray-300 border-gray-900 rounded-md"
      onClick={() => setCookieConsent(false)}
