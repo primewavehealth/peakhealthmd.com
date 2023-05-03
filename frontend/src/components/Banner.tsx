d2d4a458781db2f2c2368882bc7270d614f697c7
Peter Egbujie <ogbillion@gmail.com>
2023-05-03 11:39:33 -0700

    Edited home page text

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -17,8 +17,11 @@ function Banner() {
     <div className="max-w-4xl py-32 sm:py-48 lg:py-56">
      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
       <div className="relative px-3 py-1 text-sm leading-6 rounded-full text-neutral-800 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-       Get 15% off All Hormone Therapies. Hurry, Offer lasts tills 4/30.{" "}
-       <Link href="/contact" className="font-semibold text-indigo-600">
+       Get 15% off All IV Therapies. Hurry, Offer lasts tills 5/30.{" "}
+       <Link
+        href="/IV-therapy-las-vegas"
+        className="font-semibold text-indigo-600"
+       >
         <span className="absolute inset-0" aria-hidden="true" />
         Learn More <span aria-hidden="true">&rarr;</span>
        </Link>
@@ -26,11 +29,11 @@ function Banner() {
      </div>
      <div className="text-center">
       <h1 className="text-2xl font-bold tracking-tight text-blue-600 sm:text-6xl">
-       <span className="text-black">Welcome to</span> The Vegas Clinic
+       The Vegas Clinic
       </h1>
-      <p className="mt-6 text-sm leading-8 lg:text-xl text-neutral-800">
-       Patient-centered healthcare with excellence in quality
-      </p>
+      <h2 className="mt-6 text-sm leading-8 lg:text-xl text-neutral-800">
+       Pain Management and Wellness Center in Las Vegas
+      </h2>
       <div className="flex items-center justify-center mt-10 gap-x-6">
        <Link
         href="/telehealth"
