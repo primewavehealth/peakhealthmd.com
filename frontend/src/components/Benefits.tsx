4812cca7e5a165312855c02b21f9d02f397206ee
Peter Egbujie <ogbillion@gmail.com>
2023-05-17 17:15:54 -0700

    Added a benefits section

---
+++ frontend/src/components/Benefits.tsx
@@ -0,0 +1,301 @@
+import Image from "next/image";
+
+function benefits() {
+ return (
+  <section className="bg-white dark:bg-gray-900">
+   <div className="container max-w-screen-xl px-8 py-16 mx-auto">
+    <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+     Our <br /> Benefits
+    </h1>
+
+    <div className="mt-2">
+     <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+     <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+     <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+    </div>
+
+    <div className="mt-8 xl:mt-12 lg:flex lg:items-center">
+     <div className="grid w-full grid-cols-1 gap-8 lg:w-1/2 xl:gap-16 md:grid-cols-2">
+      <div className="space-y-3">
+       <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+        <svg
+         fill="#1a55e0"
+         height="64px"
+         width="64px"
+         version="1.1"
+         id="Layer_1"
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 329.928 329.928"
+        >
+         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+         <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+         ></g>
+         <g id="SVGRepo_iconCarrier">
+          {" "}
+          <g id="XMLID_481_">
+           {" "}
+           <path
+            id="XMLID_482_"
+            d="M115.604,278.4c5.95,5.224,12.092,10.614,18.41,16.202c0.043,0.038,0.087,0.077,0.131,0.115l21.018,18.155 c2.816,2.433,6.311,3.649,9.806,3.649s6.991-1.217,9.807-3.649l21.014-18.155c0.044-0.038,0.087-0.076,0.13-0.114 c41.607-36.796,72.802-64.966,95.371-92.44c26.36-32.088,38.638-61.101,38.638-91.305c0-54.646-42.805-97.451-97.449-97.451 c-24.56,0-48.827,9.248-67.511,25.279c-18.689-16.032-42.956-25.279-67.517-25.279C42.806,13.406,0,56.212,0,110.857 C0,176.912,45.99,217.286,115.604,278.4z M104.964,134.965h39.999V94.964h40.001v40.001h40v40h-40v39.999h-40.001v-39.999h-39.999 V134.965z"
+           ></path>{" "}
+          </g>{" "}
+         </g>
+        </svg>
+       </span>
+
+       <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+        Integrated Healthcare Approach
+       </h1>
+
+       <p className="text-gray-500 dark:text-gray-300">
+        Our medical and wellness clinic offers a unique and comprehensive
+        approach by combining medical expertise with holistic wellness
+        practices. We believe in addressing the root causes of health issues,
+        focusing on prevention, and optimizing overall well-being.
+       </p>
+      </div>
+
+      <div className="space-y-3">
+       <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+        <svg
+         width="64px"
+         height="64px"
+         viewBox="0 0 400 400"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+        >
+         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+         <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+         ></g>
+         <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+           d="M222 76C210.988 106.84 171.627 128.31 147 132"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M236 44.053C123.346 20.1218 96.7679 144.026 136.104 167"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M256 54C302.745 75.4047 288.975 108.654 272.736 144"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M260.902 122C295.577 228.082 142 250.963 142 156.601"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M218.892 153C219.298 150.031 218.46 147.754 218 145"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M191 154C191 151.332 191 148.668 191 146"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M60 345.501C60 309.522 83.3747 224.325 163.582 228.248C185.925 229.341 191.24 351.835 206.062 345.501C232 334.416 223.446 254.231 243.571 224.158C340.019 219.027 341 340.572 341 359"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M296 271C288.365 253.665 267.103 230.409 247 228"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M163 232C139.27 246.396 128.966 267.837 120 292"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M93.0228 347.996C90.4525 330.039 91.6852 307.132 109.075 296.665C157.969 267.237 151.718 362.878 128.138 345.983"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M293.07 271.039C321.891 269.785 283.781 299.392 290.907 273.038"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+          <path
+           d="M304 324.289C291.859 322.728 282.476 327.953 271 329"
+           stroke="#1a55e0"
+           stroke-opacity="0.9"
+           stroke-width="16"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+          ></path>{" "}
+         </g>
+        </svg>
+       </span>
+
+       <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+        Expert Multidisciplinary Team
+       </h1>
+
+       <p className="text-gray-500 dark:text-gray-300">
+        Our clinic is staffed with a highly qualified team of healthcare
+        professionals. Each member brings their specialized knowledge and
+        experience to provide personalized care and tailored treatment plans.
+       </p>
+      </div>
+
+      <div className="space-y-3">
+       <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+        <svg
+         width="64px"
+         height="64px"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+         stroke="#1a55e0"
+        >
+         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+         <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+         ></g>
+         <g id="SVGRepo_iconCarrier">
+          {" "}
+          <g id="Navigation / House_Close">
+           {" "}
+           <path
+            id="Vector"
+            d="M10 15L12 13.0001M12 13.0001L14 11.0001M12 13.0001L10 11.0001M12 13.0001L14 15M4 16.8002V11.4522C4 10.9179 4 10.6506 4.06497 10.4019C4.12255 10.1816 4.2173 9.97307 4.34521 9.78464C4.48955 9.57201 4.69064 9.39569 5.09277 9.04383L9.89436 4.84244C10.6398 4.19014 11.0126 3.86397 11.4324 3.73982C11.8026 3.63035 12.1972 3.63035 12.5674 3.73982C12.9875 3.86406 13.3608 4.19054 14.1074 4.84383L18.9074 9.04383C19.3096 9.39569 19.5102 9.57201 19.6546 9.78464C19.7825 9.97307 19.8775 10.1816 19.9351 10.4019C20 10.6505 20 10.9184 20 11.4522V16.8037C20 17.9216 20 18.4811 19.7822 18.9086C19.5905 19.2849 19.2842 19.5906 18.9079 19.7823C18.4805 20.0001 17.9215 20.0001 16.8036 20.0001H7.19691C6.07899 20.0001 5.5192 20.0001 5.0918 19.7823C4.71547 19.5906 4.40973 19.2849 4.21799 18.9086C4 18.4807 4 17.9203 4 16.8002Z"
+            stroke="#1a55e0"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+           ></path>{" "}
+          </g>{" "}
+         </g>
+        </svg>
+       </span>
+
+       <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+        State-of-the-Art Facilities and Technology
+       </h1>
+
+       <p className="text-gray-500 dark:text-gray-300">
+        We pride ourselves on maintaining modern facilities equipped with
+        advanced medical technologies. From diagnostic tools to cutting-edge
+        treatment modalities, we ensure that our patients have access to the
+        latest innovations in healthcare for accurate diagnoses and effective
+        interventions.
+       </p>
+      </div>
+
+      <div className="space-y-3">
+       <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+        <svg
+         fill="#1a55e0"
+         height="64px"
+         width="64px"
+         version="1.1"
+         id="Layer_1"
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 512 512"
+        >
+         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+         <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+         ></g>
+         <g id="SVGRepo_iconCarrier">
+          {" "}
+          <g>
+           {" "}
+           <g>
+            {" "}
+            <path d="M459.662,481.328l-31.078-113.353l12.551-79.88c1.508-9.598,1.23-19.393-0.819-28.891l-31.273-144.88 c-3.536-16.433-19.724-26.887-36.157-23.351c-12.682,2.729-47.545,10.231-53.06,11.418c-16.433,3.536-26.887,19.724-23.35,36.157 l3.417,15.878l26.447-28.482l-24.437,47.695l-66.525,24.87c1.107-6.231,2.811-13.263,5.423-20.521 c5.742-15.95,15.878-32.975,33.749-44.92c22.205-14.84,34.873-34.103,42.102-51.251c19.995-5.205,33.46-24.594,30.713-45.613 c-2.976-22.774-23.851-38.824-46.625-35.848c-22.774,2.976-38.824,23.851-35.848,46.625c0.022,0.175,0.056,0.345,0.082,0.519 c-6.345,13.734-16.667,42.708-21.113,92.36c-11.719,14.514-18.27,30.861-21.909,45.173c-2.063,8.114-3.191,15.569-3.801,21.667 c-1.914,1.835-3.431,4.017-4.491,6.405c-4.031-4.631-8.568-6.95-12.493-5.771c-4.662,1.4-6.66,7.044-7.272,11.935 c-2.944-1.856-6.301-3.164-9.952-3.758c-3.718-0.606-41.322-6.722-47.035-7.651c-14.567-2.368-28.359,7.508-30.665,22.086 c-13.702,86.589-16.706,106.071-19.43,130.359c-0.733,6.902,1.814,13.217,6.323,17.636H68.481 c-9.396,0-17.013,7.617-17.013,17.013v100.031c0,9.396,7.617,17.013,17.013,17.013c9.396,0,17.013-7.617,17.013-17.013v-27.975 h67.378v27.975c0,9.396,7.617,17.013,17.013,17.013c9.396,0,17.013-7.617,17.013-17.013l0.113-104.804l19.649,1.503l-15.3,95.398 c-1.88,11.727,6.102,22.759,17.829,24.641c11.726,1.879,22.759-6.102,24.641-17.83l18.999-118.466 c0.784-4.884-0.17-9.84-2.595-14.078c-0.524-0.914-1.091-1.807-1.752-2.644c-3.277-4.157-7.956-6.934-13.119-7.853 c2.161-1.857,3.918-4.249,5.021-7.091c3.583-9.227-0.993-19.611-10.219-23.193l-57.791-22.439l-12.465-56.759l21.002,40.849 l18.11,7.032L204.104,258c1.076-0.466,2.126-0.946,3.125-1.452c4.429,3.67,9.605,4.716,13.292,0.434 c2.592-3.01,3.941-8.208,3.835-14.414c0.536-0.691,1.042-1.378,1.518-2.062c4.391,1.433,9.03,1.328,13.329-0.28l84.191-31.474 c4.72-1.764,8.612-5.229,10.91-9.715l28.008-54.664l-12.59,62.563c-2.59,12.873-13.391,15.476-20.261,18.042l-13.314,4.978 l13.9,61.089l-24.72,67.398c-1.569,4.28-1.901,8.917-0.957,13.376l25.612,120.945c2.774,13.094,15.631,21.477,28.751,18.702 c13.104-2.776,21.477-15.647,18.702-28.751l-24.167-114.119c34.692-94.586,32.543-88.679,32.838-89.613l7.297-1.57l-13.545,88.313 c-0.516,3.367-0.318,6.805,0.583,10.091l32.445,118.337c3.544,12.925,16.895,20.519,29.802,16.977 C455.603,507.588,463.204,494.247,459.662,481.328z M273.968,67.952c6.187,7.534,14.92,12.773,24.692,14.555 c-13.014,25.627-30.924,35.724-36.909,40.166C265.041,98.073,269.768,80.172,273.968,67.952z M152.872,444.327H85.494v-32.36 h67.378V444.327z M202.336,348.217l-59.859-5.637c-11.738-1.106-20.692-11.015-20.573-22.804c0.002-0.242,0.009-0.482,0.018-0.724 l2.979-69.987l14.526,66.143c1.287,5.862,5.423,10.69,11.019,12.861L202.336,348.217z"></path>{" "}
+           </g>{" "}
+          </g>{" "}
+          <g>
+           {" "}
+           <g>
+            {" "}
+            <circle cx="174.682" cy="154.031" r="39.557"></circle>{" "}
+           </g>{" "}
+          </g>{" "}
+         </g>
+        </svg>
+       </span>
+
+       <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+        Patient-Centered Care
+       </h1>
+
+       <p className="text-gray-500 dark:text-gray-300">
+        At the Vegas clinic, we prioritize the needs and well-being of our
+        patients above all else. We strive to create a warm and compassionate
+        environment where individuals feel heard, respected, and actively
+        involved in their healthcare journey. Our focus is on building long-term
+        relationships based on trust, empathy, and personalized attention.
+       </p>
+      </div>
+     </div>
+
+     <div className="hidden lg:flex lg:w-1/2 lg:justify-center">
+      <Image
+       className="w-[28rem] h-[28rem] flex-shrink-0 object-cover xl:w-[34rem] xl:h-[34rem] rounded-full"
+       alt="banner"
+       src="/images/carboxy.jpg"
+       height={448}
+       width={448}
+      />
+     </div>
+    </div>
+   </div>
+  </section>
+ );
+}
+
+export default benefits;
