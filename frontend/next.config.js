a8a531104a40a0cfd722d1bd3966970c3615b09a
Peter Egbujie <ogbillion@gmail.com>
2023-07-18 13:22:55 -0700

    Added blog post

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -54,16 +54,6 @@ const securityHeaders = [
   key: "X-Frame-Options",
   value: "DENY",
  },
- // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
- {
-  key: "X-Content-Type-Options",
-  value: "nosniff",
- },
- // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
- {
-  key: "X-DNS-Prefetch-Control",
-  value: "on",
- },
  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
  {
   key: "Strict-Transport-Security",
