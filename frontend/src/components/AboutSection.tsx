5ccf9f370b8a471c116c8efb93d709e85b69d042
Peter Egbujie <ogbillion@gmail.com>
2023-08-01 16:38:35 -0700

    changed about section text

--- frontend/src/components/AboutSection.tsx
+++ frontend/src/components/AboutSection.tsx
@@ -5,7 +5,7 @@ import Button from "./UI/Button";
 function AboutSection() {
  return (
   <section className="text-blue-900">
-   <div className="mx-auto max-w-screen-2xl">
+   <div className="py-12 mx-auto max-w-screen-2xl">
     <div className="grid grid-cols-1 lg:grid-cols-2">
      <div className="relative hidden h-full lg:flex">
       <Image
@@ -19,23 +19,18 @@ function AboutSection() {
      </div>
      <div className="p-8 md:p-12 lg:px-16 lg:py-30">
       <div className="max-w-xl pt-12 mx-auto space-y-3">
-       <span className="text-xl font-semibold lg:text-3xl">
-        Technology Meets Pain Care at{" "}
-        <strong className="text-yellow-400">PrimeWave</strong>{" "}
+       <span className="text-4xl font-semibold">
+        Experience Unrivaled Pain Relief at Our Premier Care Clinic{" "}
        </span>
 
-       <p className="text-base text-justify lg:text-lg sm:mt-4">
-        Welcome to Primewave, a leading pain management clinic in{" "}
-        <Link href="https://www.lasvegasnevada.gov/">Las Vegas</Link>. We
-        specialize in providing top-notch care for all types of pain. Our clinic
-        is staffed with highly skilled pain doctors who are dedicated to
+       <p className="py-6 text-base text-justify lg:text-lg sm:mt-4">
+        Welcome to Primewave, we are a leading pain management clinic in{" "}
+        <Link href="https://www.lasvegasnevada.gov/">Las Vegas</Link>. Our
+        clinic is staffed with highly skilled pain doctors who are dedicated to
         delivering the highest standard of expertise and quality care to our
         patients. Using the latest advancements in pain management, including
         cutting-edge technologies like shockwave therapy, we tailor personalized
-        treatment plans to address each individual's unique pain concerns. With
-        a focus on non-invasive and evidence-based approaches, our pain care
-        clinic is committed to helping patients regain their quality of life and
-        find lasting relief from pain.
+        treatment plans to address each individual's unique pain concerns.
        </p>
 
        <div className="mt-4 md:mt-8">
