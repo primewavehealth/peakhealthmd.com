8625fa9b8dfe5d16e0fd97c56c602e53affd7fc9
Peter Egbujie <ogbillion@gmail.com>
2023-09-20 14:33:07 -0700

    Added the Healthy Hero page

---
+++ frontend/src/components/HeroesSection.tsx
@@ -0,0 +1,107 @@
+export default function HeroesSection() {
+ return (
+  <div className="px-4 py-16 mx-auto sm:max-w-4xl md:px-24 lg:px-8 lg:py-20">
+   <section className="">
+    <div className="max-w-screen-xl px-8 py-16 mx-auto text-lg">
+     <div className="max-w-3xl mx-auto text-base text-justify md:text-lg">
+      <h4 className="py-6 text-center">
+       {" "}
+       🌟 Calling All Super Nominators! <br />
+      </h4>
+      <h2 className="pb-4 text-3xl font-bold text-center sm:text-4xl">
+       🌟 Unleash Your Inner Hero: Nominate a Healthy Hero Today!
+      </h2>
+
+      <article className="space-y-4 text-justify text-gray-600 ">
+       <p>
+        🦸‍♀️ Have you ever known someone who's tackled pain like a champion?{" "}
+        <br />
+        🏋️‍♂️ Seen a buddy prevent injuries like a pro? <br />
+        🥗 Or watched a friend level up their lifestyle and well-being? Now's
+        your chance to shine a spotlight on them! 🌟
+       </p>
+      </article>
+     </div>
+     <div className="py-6 text-4xl font-bold text-center lg:py-12">
+      Welcome to the Healthy Heroes Nomination Campaign brought to you by
+      PrimeWave Medical Clinic of Las Vegas!
+     </div>
+     <div className="max-w-3xl pt-10 mx-auto text-base text-justify md:text-lg">
+      <h2 className="pb-4 text-3xl font-bold text-center sm:text-4xl">
+       What's Healthy Heroes All About?
+      </h2>
+
+      <article className="space-y-4 text-justify text-gray-600 ">
+       <p>
+        At Healthy Heroes, we're on a mission to save the day by spreading the
+        word on three super-powered aspects of well-being: <br />
+        1. 💪 Pain Management: Learn to conquer discomfort and seize control of
+        your life!
+        <br />
+        2. 🏆 Athletic Injury Prevention: Dodge those injuries and stay in the
+        game like a true MVP! <br />
+        3. 🌟 Lifestyle Improvement: Elevate your everyday choices to unleash
+        your inner superhero!
+       </p>
+      </article>
+     </div>
+     <div className="max-w-3xl pt-10 mx-auto text-base text-justify md:text-lg">
+      <h2 className="pb-4 text-3xl font-bold text-center sm:text-4xl">
+       Why Nominate a Healthy Hero?
+      </h2>
+
+      <article className="space-y-4 text-justify text-gray-600 ">
+       <p>
+        👑 Because heroes come in all shapes and sizes! Your friends, family,
+        neighbors, or even your trusty sidekick could be the next Healthy Hero.
+        By nominating them, you're not only shining a spotlight on their amazing
+        journey but also inspiring others to take the leap into well-being
+        greatness!
+       </p>
+      </article>
+     </div>
+     <div className="max-w-3xl pt-10 mx-auto text-base text-justify md:text-lg">
+      <h2 className="pb-4 text-3xl font-bold text-center sm:text-4xl">
+       How to Nominate Your Hero:
+      </h2>
+
+      <article className="space-y-4 text-justify text-gray-600 ">
+       <p>
+        1.🚀 Complete this super-easy nomination form. <br />
+        2. 🦸‍♂️ Tell us why your nominee is a Healthy Hero. Did they defeat pain,
+        outsmart injuries, or level up their lifestyle?
+        <br /> 3. 💥 Send it our way, and watch the magic happen!
+       </p>
+      </article>
+     </div>
+     <div className="max-w-3xl pt-10 mx-auto text-base text-justify md:text-lg">
+      <h2 className="pb-4 text-3xl font-bold text-center sm:text-4xl">
+       What's in it for Your Hero?
+      </h2>
+
+      <article className="space-y-4 text-justify text-gray-600 ">
+       <p>
+        The chance to become a *REAL-LIFE HEALTHY HERO* with a spotlight
+        feature, epic recognition, and an invitation to our exclusive Hero
+        Celebration Event! 🎉 Plus, they'll inspire countless others on their
+        own well-being journeys.
+       </p>
+      </article>
+     </div>
+     <div className="max-w-3xl pt-10 mx-auto text-base text-justify md:text-lg">
+      <h2 className="pb-4 text-3xl font-bold text-center sm:text-4xl">
+       Join the Healthy Heroes League Today!
+      </h2>
+
+      <article className="space-y-4 text-justify text-gray-600 ">
+       <p>
+        📢 Don't wait—nominate your hero today and let's make Las Vegas a
+        healthier, happier place, one hero at a time! 💥
+       </p>
+      </article>
+     </div>
+    </div>
+   </section>
+  </div>
+ );
+}
