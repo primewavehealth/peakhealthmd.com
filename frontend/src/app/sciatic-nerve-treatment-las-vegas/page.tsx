f314eb0c4feaa1a5748465f4a838c69f0c61fd37
Peter Egbujie <ogbillion@gmail.com>
2023-11-14 13:41:28 -0800

    Added sciatica page

---
+++ frontend/src/app/sciatic-nerve-treatment-las-vegas/page.tsx
@@ -0,0 +1,430 @@
+import AppointmentForm from "@/components/AppointmentForm";
+import CTA from "@/components/UI/CTA";
+import PageBanner from "@/components/UI/PageBanner";
+import type { Metadata } from "next";
+import Link from "next/link";
+import pain from "/public/images/pain.jpg";
+
+export const metadata: Metadata = {
+ title: "Sciatica Nerve Treatment in Las Vegas",
+ description:
+  "Comprehensive sciatica nerve treatment in Las Vegas. Our pain doctors deliver personalized treatments for lasting comfort and recovery.",
+ alternates: {
+  canonical: "/sciatic-nerve-treatment-las-vegas",
+ },
+};
+
+function page() {
+ return (
+  <div>
+   <PageBanner
+    heading="Sciatic Nerve Treatment in Las Vegas"
+    body="
+Are you tired of letting sciatic nerve pain dictate your daily life? Primewave Health offers personalized and effective sciatica nerve treatment in Las Vegas that can bring relief and restore your quality of life. Our pain doctors specializes in tailoring treatment plans to address the root causes of your sciatic pain. Utilizing cutting-edge diagnostic tools, we ensure accurate diagnoses to guide targeted interventions."
+    src={pain}
+    height={400}
+    width={400}
+    alt="chronic-pain"
+    btn={{
+     href: "/appointment",
+     text: "Schedule An Appointment",
+     id: "schedule",
+
+     icon: (
+      <svg
+       fill="none"
+       stroke="currentColor"
+       strokeLinecap="round"
+       strokeLinejoin="round"
+       strokeWidth="2"
+       className="w-4 h-4 ml-auto"
+       viewBox="0 0 24 24"
+      >
+       <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+     ),
+    }}
+   />
+   {/* body */}
+
+   <div className="container py-8 mx-auto">
+    <article className="p-8 leading-8 md:pl-24">
+     <div className="flex flex-col md:flex-row">
+      <div className="w-full md:pr-8">
+       <h2 className="mb-4 text-2xl font-bold">
+        Sciatica Nerve: A Comprehensive Guide
+       </h2>
+       <div className="mb-4">
+        <h3 className="mb-2 text-xl font-bold">What is sciatica?</h3>
+        <p className="py-6 text-lg font-medium leading-loose text-gray-500 md:text-xl">
+         <Link
+          href="https://www.hopkinsmedicine.org/health/conditions-and-diseases/sciatica"
+          className="underline-offset-4"
+         >
+          Sciatica
+         </Link>{" "}
+         is a painful condition that affects millions of people worldwide. It is
+         caused by irritation or compression of the sciatic nerve, the largest
+         nerve in the body. The sciatic nerve runs from the{" "}
+         <Link href="/back-pain-las-vegas" className="underline-offset-4">
+          lower back
+         </Link>{" "}
+         down the buttocks and legs. When the sciatic nerve is irritated or
+         compressed, it can cause pain, numbness, tingling, and weakness in the
+         leg.
+        </p>
+       </div>
+       <div className="mb-4">
+        <h3 className="mb-2 text-xl font-bold">
+         Prevalence and Impact of sciatica
+        </h3>
+        <p>
+         Sciatica is a common condition, affecting{" "}
+         <Link
+          href="https://www.health.harvard.edu/pain/sciatica-of-all-the-nerve"
+          className="underline-offset-4"
+         >
+          up to 40%
+         </Link>{" "}
+         of people at some point in their lives. It is more common in people
+         between the ages of 30 and 50. Sciatica can have a significant impact
+         on quality of life, making it difficult to work, exercise, and enjoy
+         everyday activities.
+        </p>
+       </div>
+       <div className="mb-4">
+        <h3 className="mb-2 text-xl font-bold">Anatomy of the Sciatic Nerve</h3>
+        <p>
+         The sciatic nerve is formed by the roots of the L4, L5, S1, and S2
+         spinal nerves. It exits the pelvis through the greater sciatic foramen
+         and runs down the back of the thigh. The sciatic nerve branches into
+         smaller nerves that supply the muscles, skin, and joints of the leg and
+         foot.
+        </p>
+       </div>
+      </div>
+      <div className="w-full md:-mt-16 md:w-1/3">
+       <AppointmentForm />
+      </div>
+     </div>
+    </article>
+
+    <article className="p-8 prose md:-mt-9 md:pl-24 max-w-none md:w-2/3">
+     <h2 className="mb-4 text-2xl font-bold">
+      Causes and Symptoms of Sciatica
+     </h2>
+     <div className="mb-4">
+      <h3 className="mb-2 text-xl font-bold">
+       What are the causes of sciatica?
+      </h3>
+      <p>
+       There are many possible{" "}
+       <Link
+        href="https://www.mayoclinic.org/diseases-conditions/sciatica/symptoms-causes/syc-20377435"
+        className="underline-offset-4"
+       >
+        causes of sciatica
+       </Link>{" "}
+       , but the most common are:
+      </p>
+      <ul className="pl-4 mb-8 list-disc">
+       <li className="mb-2">
+        Herniated disc: A herniated disc occurs when the inner part of a spinal
+        disc ruptures and pushes out through the outer part of the disc. This
+        can put pressure on the sciatic nerve and cause pain.
+       </li>
+       <li className="mb-2">
+        Lumbar spinal stenosis: Lumbar spinal stenosis is a narrowing of the
+        spinal canal in the lower back. This can put pressure on the sciatic
+        nerve and cause pain.
+       </li>
+       <li className="mb-2">
+        Degenerative disc disease: Degenerative disc disease is a condition in
+        which the discs in the spine break down and become more susceptible to
+        injury. This can lead to a herniated disc or other problems that can
+        compress the sciatic nerve.
+       </li>
+       <li className="mb-2">
+        Piriformis syndrome: Piriformis syndrome is a condition in which the
+        piriformis muscle, located in the buttocks, spasms and compresses the
+        sciatic nerve.
+       </li>
+      </ul>
+      <p>Less common causes of sciatica include:</p>
+      <ul className="pl-4 mb-8 list-disc">
+       <li className="mb-2">
+        Spondylolisthesis: Spondylolisthesis is a condition in which a vertebra
+        slips forward on the vertebra below it. This can put pressure on the
+        sciatic nerve and cause pain.
+       </li>
+       <li className="mb-2">
+        Spinal tumor: A spinal tumor can compress the sciatic nerve and cause
+        pain.
+       </li>
+       <li className="mb-2">
+        Cauda equina syndrome: Cauda equina syndrome is a rare but serious
+        condition in which the nerve roots at the end of the spinal cord are
+        compressed. This can cause pain, numbness, weakness, and loss of bladder
+        or bowel control.
+       </li>
+       <li className="mb-2">
+        Sacroiliac joint dysfunction: Sacroiliac joint dysfunction is a
+        condition in which the sacroiliac joints, located at the base of the
+        spine, are misaligned or inflamed. This can cause pain in the buttocks
+        and legs.
+       </li>
+       <li className="mb-2">
+        Pregnancy: Pregnancy can cause changes in the spine that can put
+        pressure on the sciatic nerve and cause pain.
+       </li>
+      </ul>
+     </div>
+
+     <div className="mb-4">
+      <h3 className="mb-2 text-xl font-bold">
+       What are the symptoms of sciatica?
+      </h3>
+      <p>
+       The primary symptom of sciatica is pain along the path of the sciatic
+       nerve. The pain is typically described as sharp, burning, or tingling. It
+       may start in the lower back and radiate down the buttocks, leg, and foot.
+       The pain may be worse with certain activities, such as sitting, bending,
+       or lifting. Other common symptoms of sciatica include:
+      </p>
+      <ul className="pl-4 mb-8 list-disc">
+       <li className="mb-2">Numbness or tingling in the leg or foot</li>
+       <li className="mb-2">Weakness in the leg</li>
+       <li className="mb-2">Burning sensation in the leg or foot</li>
+       <li className="mb-2">Loss of bladder or bowel control</li>
+      </ul>
+     </div>
+    </article>
+    <article className="p-8 prose md:pl-24 max-w-none md:w-2/3">
+     <h2 className="mb-4 text-2xl font-bold">
+      Diagnosis and Treatment of Sciatica
+     </h2>
+     <div className="mb-4">
+      <h3 className="mb-2 text-xl font-bold">Diagnosis of Sciatica </h3>
+      <p>
+       There is no single test that can diagnose sciatica. The diagnosis is
+       usually made based on a physical examination and a discussion of the
+       patient's symptoms. In some cases, diagnostic imaging tests, such as
+       X-rays, MRI, or CT scans, may be ordered to help rule out other causes of
+       pain.
+      </p>
+     </div>
+     <div className="mb-4">
+      <h3 className="mb-2 text-xl font-bold">Treatment Options for Sciatica</h3>
+      <p>
+       Treatment for sciatica depends on the severity of the symptoms and the
+       underlying cause. In most cases, non-invasive treatments, such as rest,
+       ice and heat therapy, over-the-counter pain relievers, exercise, and
+       chiropractic care, can relieve pain and improve symptoms. In some cases,
+       epidural steroid injections or surgery may be necessary.
+      </p>
+     </div>
+     <div className="mb-4">
+      <h3 className="mb-2 text-xl font-bold">Prevention of Sciatica</h3>
+      <p>
+       There are a number of things that can be done to reduce the risk of
+       sciatica, including:
+      </p>
+      <ul className="pl-4 mb-8 list-disc">
+       <li className="mb-2">Maintaining a healthy weight</li>
+       <li className="mb-2">Regular exercise</li>
+       <li className="mb-2">Proper</li>
+      </ul>
+     </div>
+    </article>
+   </div>
+
+   {/* next article */}
+   <CTA
+    heading="Help for sciatica nerve pain"
+    body="Discover relief from sciatic nerve pain at Primewave Health in Las Vegas. Our pain doctors offers personalized treatment plans, utilizing advanced diagnostics to target the root causes of your discomfort. From non-invasive options like chiropractic care and shockewave therapy to advanced interventions including injections, we provide comprehensive solutions.Take control of your life again – find relief at Primewave Health in Las Vegas."
+    src="/images/happy-man.avif"
+    alt="Chronic Pain Treatment in Las Vegas"
+    btn={{
+     href: "/appointment",
+     text: "Schedule An Appointment",
+     id: "cta",
+
+     icon: (
+      <svg
+       fill="none"
+       stroke="currentColor"
+       strokeLinecap="round"
+       strokeLinejoin="round"
+       strokeWidth="2"
+       className="w-4 h-4 ml-auto"
+       viewBox="0 0 24 24"
+      >
+       <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+     ),
+    }}
+   />
+
+   {/* faqs*/}
+   <article className="max-w-3xl mx-auto my-12">
+    <h2 className="py-4 text-center">
+     Frequently Asked Questions About Sciatica
+    </h2>
+    <div className="space-y-4">
+     <details
+      className="group border-s-4 border-yellow-400 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+      open
+     >
+      <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+       <h3 className="text-lg font-bold text-gray-900">
+        What are the common risk factors for developing sciatica?
+       </h3>
+
+       <span className="shrink-0 rounded-full  p-1.5 text-gray-900 sm:p-3">
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         className="w-5 h-5 transition duration-300 shrink-0 group-open:-rotate-45"
+         viewBox="0 0 20 20"
+         fill="currentColor"
+        >
+         <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+         />
+        </svg>
+       </span>
+      </summary>
+
+      <p className="mt-4 text-lg leading-relaxed text-gray-700">
+       Risk factors include age-related changes in the spine, obesity,
+       occupations requiring heavy lifting or prolonged sitting, diabetes, and
+       certain lifestyle factors. Pregnancy can also contribute to sciatica due
+       to increased pressure on the sciatic nerve.
+      </p>
+     </details>
+
+     <details className="group border-s-4 border-yellow-400 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+       <h3 className="text-lg font-bold text-gray-900">
+        Can sciatica be prevented?
+       </h3>
+
+       <span className="shrink-0 rounded-full  p-1.5 text-gray-900 sm:p-3">
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         className="w-5 h-5 transition duration-300 shrink-0 group-open:-rotate-45"
+         viewBox="0 0 20 20"
+         fill="currentColor"
+        >
+         <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+         />
+        </svg>
+       </span>
+      </summary>
+
+      <p className="mt-4 text-lg leading-relaxed text-gray-700">
+       While it may not be entirely preventable, maintaining a healthy weight,
+       practicing good posture, avoiding prolonged sitting or standing, and
+       engaging in regular exercise can help reduce the risk of developing
+       sciatica.
+      </p>
+     </details>
+     <details className="group border-s-4 border-yellow-400 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+       <h3 className="text-lg font-bold text-gray-900">
+        What exercises can help alleviate sciatica pain?
+       </h3>
+
+       <span className="shrink-0 rounded-full  p-1.5 text-gray-900 sm:p-3">
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         className="w-5 h-5 transition duration-300 shrink-0 group-open:-rotate-45"
+         viewBox="0 0 20 20"
+         fill="currentColor"
+        >
+         <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+         />
+        </svg>
+       </span>
+      </summary>
+
+      <p className="mt-4 text-lg leading-relaxed text-gray-700">
+       Exercises that focus on stretching and strengthening the lower back,
+       buttocks, and legs can be beneficial. Examples include the cat-cow
+       stretch, knee-to-chest stretch, and piriformis stretch. However, it's
+       crucial to consult with a healthcare professional before starting any
+       exercise program.
+      </p>
+     </details>
+     <details className="group border-s-4 border-yellow-400 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+       <h3 className="text-lg font-bold text-gray-900">
+        How long does it typically take for sciatica to resolve?
+       </h3>
+
+       <span className="shrink-0 rounded-full  p-1.5 text-gray-900 sm:p-3">
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         className="w-5 h-5 transition duration-300 shrink-0 group-open:-rotate-45"
+         viewBox="0 0 20 20"
+         fill="currentColor"
+        >
+         <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+         />
+        </svg>
+       </span>
+      </summary>
+
+      <p className="mt-4 text-lg leading-relaxed text-gray-700">
+       The duration of sciatica recovery varies. In many cases, symptoms improve
+       with conservative treatments within a few weeks. However, some
+       individuals may experience persistent or recurrent symptoms, requiring
+       ongoing management.
+      </p>
+     </details>
+     <details className="group border-s-4 border-yellow-400 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+       <h3 className="text-lg font-bold text-gray-900">
+        Are there complications associated with untreated sciatica?
+       </h3>
+
+       <span className="shrink-0 rounded-full  p-1.5 text-gray-900 sm:p-3">
+        <svg
+         xmlns="http://www.w3.org/2000/svg"
+         className="w-5 h-5 transition duration-300 shrink-0 group-open:-rotate-45"
+         viewBox="0 0 20 20"
+         fill="currentColor"
+        >
+         <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+         />
+        </svg>
+       </span>
+      </summary>
+
+      <p className="mt-4 text-lg leading-relaxed text-gray-700">
+       If left untreated, sciatica can lead to chronic pain, muscle weakness,
+       and in severe cases, loss of bladder or bowel control. Seeking prompt
+       medical attention and following recommended treatments can help prevent
+       complications and improve long-term outcomes.
+      </p>
+     </details>
+    </div>
+   </article>
+  </div>
+ );
+}
+
+export default page;
