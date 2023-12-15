4532d2b78f17fcc79efb27096d31d4fcb7c4896e
Peter Egbujie <ogbillion@gmail.com>
2023-12-14 21:01:13 -0800

    Fixed nav bar error on mobile

--- frontend/src/app/jobs/page.tsx
+++ frontend/src/app/jobs/page.tsx
@@ -38,7 +38,7 @@ function page() {
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth="2"
          >
           <path
            stroke-linecap="round"
@@ -64,7 +64,7 @@ function page() {
          fill="none"
          viewBox="0 0 24 24"
          stroke="currentColor"
-         stroke-width="2"
+         strokeWidth="2"
         >
          <path
           stroke-linecap="round"
--- frontend/src/components/OfficeSpaceSlider.jsx
+++ frontend/src/components/OfficeSpaceSlider.jsx
@@ -1,9 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
 
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { images } from "@/lib/images.js";
@@ -14,20 +13,15 @@ import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
 export default function OfficeSpaceSlider() {
- const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
  return (
   <section className="min-h-screen py-12 bg-black">
    <div className="container">
     <Swiper
-     loop={true}
-     spaceBetween={10}
-     navigation={true}
-     thumbs={{
-      swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-     }}
-     modules={[FreeMode, Navigation, Thumbs]}
-     className="w-full rounded-lg h-96"
+     navigation
+     pagination={{ type: "fraction" }}
+     modules={[Navigation, Pagination]}
+     onSwiper={(swiper) => console.log(swiper)}
+     className="w-full rounded-lg h-[700px]"
     >
      {images.map((image, index) => (
       <SwiperSlide key={index}>
@@ -38,30 +32,6 @@ export default function OfficeSpaceSlider() {
          className="block object-cover w-full h-full"
         />
        </div>
-      </SwiperSlide>
-     ))}
-    </Swiper>
-
-    {/* Thumbnail */}
-    <Swiper
-     onSwiper={setThumbsSwiper}
-     loop={true}
-     spaceBetween={12}
-     slidesPerView={4}
-     freeMode={true}
-     watchSlidesProgress={true}
-     modules={[FreeMode, Navigation, Thumbs]}
-     className="w-full h-32 mt-3 rounded-lg thumbs"
-    >
-     {images.map((image, index) => (
-      <SwiperSlide key={index}>
-       <button className="flex items-center justify-center w-full h-full">
-        <Image
-         src={image.src}
-         alt={image.alt}
-         className="block object-cover w-full h-full"
-        />
-       </button>
       </SwiperSlide>
      ))}
     </Swiper>
