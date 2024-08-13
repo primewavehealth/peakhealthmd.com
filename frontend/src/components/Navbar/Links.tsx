fd6e062908af7fb3126db3ae78a5cb0c9dc308e5
dmb1o3 <dbarnfield1o3@gmail.com>
2024-08-13 12:58:16 -0700

    Updated links so all list of links used by navbar are in there

--- frontend/src/components/Navbar/Links.tsx
+++ frontend/src/components/Navbar/Links.tsx
@@ -1,53 +1,80 @@
+import { PhoneIcon, PlayCircleIcon } from "@heroicons/react/24/outline";
+
 export const Pain_Conditions = [
  {
   href: "/pain-management-clinics-in-las-vegas",
   description: "",
   name: "Pain Management",
+  id: "Pain Condtion Pain Management",
  },
  {
   href: "/chronic-pain-las-vegas",
   description: "",
   name: "Chronic Pain",
+  id: "Pain Condition Chronic Pain",
  },
  {
   href: "/back-pain-las-vegas",
   description: "",
   name: "Back Pain",
+  id: "Pain Condition Back Pain",
  },
  {
   href: "/podiatrist-foot-doctor-las-vegas",
   description: "",
   name: "Foot Pain",
+  id: "Pain Condition Foot Pain",
  },
  {
   href: "/joint-pain-las-vegas",
   description: "",
   name: "Joint Pain",
+  id: "Pain Condition Joint Pain",
  },
  {
   href: "/neck-pain-las-vegas",
   description: "",
   name: "Neck Pain",
+  id: "Pain Condition Neck Pain",
  },
  {
   href: "/knee-pain-las-vegas",
   description: "",
   name: "Knee Pain",
+  id: "Pain Condition Knee Pain",
  },
  {
   href: "/sciatic-nerve-treatment-las-vegas",
   description: "",
   name: "Sciatica",
+  id: "Pain Condition Sciatica",
  },
  {
   href: "/tmj-treatment-in-las-vegas",
   description: "",
   name: "TMJ",
+  id: "Pain Condition TMJ",
  },
  {
   href: "/fibromyalgia-treatment-las-vegas",
   description: "",
   name: "Fibromyalgia",
+  id: "Pain Condition Fibromyalgia",
+ },
+];
+
+export const Calls_to_Action = [
+ {
+  name: "Find A Specialist",
+  href: "/appointment",
+  icon: PlayCircleIcon,
+  id: "CTA Find A Specialist",
+ },
+ {
+  name: "Contact Us",
+  href: "/contact",
+  icon: PhoneIcon,
+  id: "CTA Contact Us",
  },
 ];
 
@@ -56,26 +83,31 @@ export const Treatments = [
   href: "/erectile-dysfunction-treatment-las-vegas",
   description: "",
   name: "Erectile Dysfunction",
+  id: "Treatments Erectile Dysfunction",
  },
  {
   href: "/low-testosterone-treatment-las-vegas",
   description: "",
   name: "Low Testosterone Treatment",
+  id: "Treatments Low Testosterone Treatment",
  },
  {
   href: "/shockwave-therapy-las-vegas",
   description: "",
   name: "Shock Wave Therapy",
+  id: "Treatments Shock Wave Therapy",
  },
  {
   href: "/weight-loss-las-vegas",
   description: "",
   name: "Weight Loss",
+  id: "Treatments Weight Loss",
  },
  {
   href: "/IV-therapy-las-vegas",
   description: "",
   name: "IV Therapy",
+  id: "Treatments IV Therapy",
  },
 ];
 
@@ -122,3 +154,25 @@ export const Womens_Health = [
   name: "IV Therapy",
  },
 ];
+
+export const Navigation = {
+ pages: [
+  { name: "About", href: "about-primewave", id: "Navigation About" },
+  { name: "Blog", href: "blog", id: "Navigation Blog" },
+
+  {
+   name: "Franchise",
+   target: "_blank",
+   href: "https://www.primewavefranchise.com/",
+   rel: "noopener noreferrer",
+   id: "Navigation Franchise",
+  },
+  {
+   name: "Medical Radio Show",
+   target: "_blank",
+   href: "https://www.americasfavoritedoctorandnurse.com/",
+   rel: "noopener noreferrer",
+   id: "Navigation Medical Radio Show",
+  },
+ ],
+};
