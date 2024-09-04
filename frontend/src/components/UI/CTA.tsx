10618b6653efbde882e226b0a4cd076014a4eb0f
dmb1o3 <dbarnfield1o3@gmail.com>
2024-09-04 14:10:34 -0700

    Removed dark mode for CTA

--- frontend/src/components/UI/CTA.tsx
+++ frontend/src/components/UI/CTA.tsx
@@ -10,10 +10,10 @@ export interface imageProps {
 function CTA(allprops: CTAConfig & imageProps) {
  const { body, heading, subheading, src, alt, btn } = allprops;
  return (
-  <div className="bg-gradient-to-t from-blue-100 to-blue-50 dark:bg-gray-900">
+  <div className="bg-gradient-to-t from-blue-100 to-blue-50">
    <div className="flex flex-col justify-between max-w-screen-xl py-8 mx-auto lg:flex-row">
     <div className="flex flex-col px-8 py-12 mx-auto lg:w-2/3 text-start lg:py-8 ">
-     <h2 className="text-2xl font-extrabold text-black dark:text-white sm:text-3xl">
+     <h2 className="text-2xl font-extrabold text-black sm:text-3xl">
       <span className="block">{heading}</span>
      </h2>
      {subheading && <h3 className="pt-2 text-xl">{subheading}</h3>}
